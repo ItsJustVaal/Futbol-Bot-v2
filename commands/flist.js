6c0bd51007f379800e6cb5fb7aecab9846ebe0c1
ItsJustVaal <109011972+ItsJustVaal@users.noreply.github.com>
2022-10-26 10:45:34 -0600

    Fixed fixture list to use UTC instead of MST

--- commands/flist.js
+++ commands/flist.js
@@ -1,77 +1,67 @@
-// This is used to update the database json files, only I (vaal) can do this
-
-// What we need:
-
-//Tables and Fixtures for:
-//PL, EL, Eredivisie, Bundesliga, LaLiga, SPL, Serie A, CL, WC, H2H 
-//'PL', 'EL', 'ER', 'BL', 'LL', 'SPL', 'SA', 'CL', 'WC', 'H2H'
-//39, 3, 88, 78, 140, 179, 71, 2, 1
-
-//Fixtures will feed:
-//.fixtures - this will change to just pull from the json file to update based on the game week
-
-// What can i do with this data?:
-// Show fixtures for the current game week
-// Set fixtures for betting
-// Results tables and auto calculating betting scores
-
-//Formats:
-//PL `Regular Season - num`
-//CL `Group Letter - num`
 module.exports = {
     name: "flist",
     execute(message) {
-        const fs = require("fs");
-        const { EmbedBuilder } = require('discord.js');
-        const leagues = ['PL', 'EL', 'ER', 'BL', 'LL', 'SPL', 'SA', 'CL', 'WC', 'H2H'];
-        const msg = message.content.split(' ')
-        const league = msg[2].toUpperCase();
-        console.log(league)
-
-        if (msg.length != 3 || leagues.includes((league)) == false || Number.isInteger(Number.parseInt(msg[1])) == false) {
-            let embed = new EmbedBuilder().addFields({ name: "Code", value: `Try the right syntax 
-            .flist GW LEAGUE | EXAMPLE: .flist 5 PL
-            League Codes:
-            PL = Premiere League
-            EL = Europa League
-            ER = Eredvisie
-            BL = Bundesliga
-            LL = LaLiga
-            SPL = Scottish Premiereship
-            SA = Serie A
-            CL = Champions League
-            WC = World Cup
-            `})
-            message.reply({ embeds: [embed] })
-        }else{
-        
-            
+            const fs = require("fs");
+            const { EmbedBuilder } = require('discord.js');
+            const leagues = ['PL', 'EL', 'ER', 'BL', 'LL', 'SPL', 'SA', 'CL', 'WC', 'H2H'];
+            const msg = message.content.split(' ')
             const weekNum = msg[1];
-            const join = fs.readFileSync(`jsons/leagues/${league}.json`);
-            const data = JSON.parse(join);
-            const lastWeek = data.response[(Object.keys(data.response).length - 1)].league.round
-            const final = lastWeek.split(' ')
-
-            if (weekNum > 0 && weekNum <= Number.parseInt(final[3])){
-                const newfile = data['response'].filter(item => item.league.round.endsWith(` ${weekNum}`) === true);
-
-                let gameweekTable = [];
-                newfile.forEach(item => {gameweekTable.push(`${item.teams.home.name} - ${item.teams.away.name}`)}); 
-
-
-                let embed = new EmbedBuilder()
-                    .setTitle(`Fixtures for ${league} Gameweek ${weekNum}`)
-                    .addFields(
-                        { name: 'H: Home | A: Away', value: gameweekTable.join('\n')}
-                    )
-                    .setColor('#1900ff')
-
+            const league = msg[2];
+            
+            
+            if (msg.length != 3 || leagues.includes((league.toUpperCase())) == false || Number.isInteger(Number.parseInt(msg[1])) == false) {
+                let embed = new EmbedBuilder().addFields({ name: "Code", value: `Try the right syntax 
+                .flist GW LEAGUE | EXAMPLE: .flist 5 PL
+                League Codes:
+                PL = Premiere League
+                EL = Europa League
+                ER = Eredvisie
+                BL = Bundesliga
+                LL = LaLiga
+                SPL = Scottish Premiereship
+                SA = Serie A
+                CL = Champions League
+                WC = World Cup
+                `})
                 message.reply({ embeds: [embed] })
-                
-                console.log('Sent Fixture list')
-            }else {
-                message.reply(`Game Week Doesnt Exist, Max Scheduled Game Week: ${final[3]}`)
+            }else{
+                const join = fs.readFileSync(`jsons/leagues/${league}.json`);
+                const data = JSON.parse(join);
+                let yellow = [];
+                let grey = [];
+                let orange = [];
+                let gameweek = [];
+                const newfile = data['response'].filter(item => item.league.round.endsWith(` ${weekNum}`) === true);
+                const lastWeek = data.response[(Object.keys(data.response).length - 1)].league.round
+                const final = lastWeek.split(' ')
+
+                if (weekNum > 0 && weekNum <= Number.parseInt(final[3])){    
+                    
+                    newfile.forEach(element => {
+                        yellow.push(element.fixture.date)
+                    });
+
+                    let purple = [...new Set(yellow)]
+                    purple.forEach(item => orange.push(item.slice(0,16).replace('T', ' T: ')))
+                    purple.forEach(item => grey.push(newfile.filter(list => list.fixture.date == item)))
+
+                    for (let i = 0; i < grey.length; i++){
+                        for(let j = 0; j < grey[i].length; j++)
+                        gameweek.push(`${grey[i][j].teams.home.name} - ${grey[i][j].teams.away.name} | ${orange[i]}` )
+                    }
+
+                    let embed = new EmbedBuilder()
+                        .setTitle(`Fixtures for ${league} Gameweek ${weekNum}`)
+                        .addFields(
+                            { name: 'H: Home | A: Away | Day/Month | 24 hr time UTC', value: gameweek.join('\n')}
+                        )
+                        .setColor('#1900ff')
+
+                    message.reply({ embeds: [embed] })}
+                else{
+                    message.react('<:pepeLoser:833719194641236068>')
+                    message.reply(`Game Week Doesnt Exist, Max Scheduled Game Week: ${final[3]}`)
+                }
             }
-        }
     }
 }
